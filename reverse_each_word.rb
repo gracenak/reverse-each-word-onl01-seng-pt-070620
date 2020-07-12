6b7f87da2e8cff923ed4d2c1e1c55496f0f74ac4
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 16:06:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,11 @@
+require 'pry'
 def reverse_each_word(array)
   new_array = sentence.split('')
   array = []
   array << new_array.each {|char| char.reverse!}
 end
   return reversed_array.join ("")
+  binding.pry
 end
     
     
