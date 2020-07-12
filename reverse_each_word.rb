cf5a87c1085a8f4bd620cfb8b23eec75a16d7a78
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 16:09:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ require 'pry'
 def reverse_each_word(array)
   array = []
   new_array = sentence.split('')
-  array << new_array.each {|char| char.reverse!}
+  array << new_array.each {|char|char.reverse!}
 end
-  return reversed_array.join ("")
+  return array.join ("")
   binding.pry
 end
     
