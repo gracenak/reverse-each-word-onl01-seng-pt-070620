8a69a4cc0854b6416c55756110b9edcd82bf9478
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 13:51:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence.split.each {|word| 
+  sentence.split.each {|word| word.reverse}
   sentence.each do |reverse|
     phrase << "Hello there, and how are you?" 
   end
