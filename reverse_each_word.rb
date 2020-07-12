c4530200ed3b2e97175502481708011f76ea30d3
Yuiko Grace Nakano <gracenak@gmail.com>
2020-07-12 13:54:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   phrase = []
-  sentence.split.each 
-    phrase << {|word| word.reverse}.join(',')
+  sentence.split.each do {|word| word.reverse}.join(',')
+    phrase << 
   end
     phrase 
-  end
+end
     
     
